407fd762ca494a7b3ae6ef3ae532de2c71c3afc5
LuizPK <luizpkpinheiro@gmail.com>
2022-09-05 15:31:07 -0300

    Resolução de exercicios aula 011 JS

---
+++ exercicio1/script.js
@@ -0,0 +1,26 @@
+const sacolao = []
+
+const fruta1 = {
+    nome:"Abacate", 
+    preço: 3.99,
+    disponibilidade: true}
+
+const fruta2 = {
+    nome:"Melancia", 
+    preço: 10.00,
+    disponibilidade: true}
+
+const fruta3 = {
+        nome:"Abacaxi", 
+        preço: 5.99,
+        disponibilidade: true}    
+
+sacolao.push[fruta1,fruta2,fruta3]
+
+console.log(sacolao)
+
+
+
+
+
+
