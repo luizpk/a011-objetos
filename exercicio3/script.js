df1c257b6a17202097769561563c693a4fd09a22
LuizPK <luizpkpinheiro@gmail.com>
2022-10-17 11:30:36 -0300

    Revisando e atualizando código dos exercícios propostos

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,50 +1,80 @@
+// # Exercício 3
 
-//a) Crie uma cópia do objeto, acima, mantendo a estrutura, mas trocando **`nome`** por “Squirtle” e **`tipo`** por “Água”
+// Observe o objeto abaixo e, utilizando o **espalhamento** resolva os exercícios a seguir:
+
+// ```jsx
 const pokemon1 = {
 	nome: "Bulbasaur",
 	tipo: "Grama",
 	nivel: 5
-}
+};
+// ```
 
-const copiaPokemon1 = {...pokemon1}
-copiaPokemon1.nome = "Squirtle"
-copiaPokemon1.tipo = "Água"
+// a) Crie uma cópia do objeto, acima, mantendo a estrutura, mas trocando **`nome`** por “Squirtle” e **`tipo`** por “Água”
+
+const pokemon2 = {
+    ...pokemon1,
+    nome:"Squirtle",
+    tipo: "Água"
+}
+//console.log(pokemon2)
 
 
 //b) No objeto original, adicione mais uma propriedade, chamada `ataques`. Esta propriedade deve guardar um **array** de objetos com a estrutura abaixo. Inicie a propriedade como um array vazio, e crie o objeto abaixo utilizando `**push()**`
+
 pokemon1.ataques = []
+
+
+// const ataque1 = {
+//     nome: "Investida",
+//  dano: 40,
+//  tipo: "Normal",
+//  precisao: 100
+// }
+
+// pokemon1.ataques.push(ataque1)
+
 pokemon1.ataques.push({
     nome: "Investida",
-    dano: "40 de dano",
-    tipo: "Normal",
-    precisão: 100,
+ dano: 40,
+ tipo: "Normal",
+ precisao: 100
 })
 
-//c) Na cópia do objeto original, adicione a propriedade **`ataques`** a partir do **espalhamento** da propriedade criada para o objeto original;
 
-copiaPokemon1.ataques = [...pokemon1.ataques]
+//console.log(pokemon1)
 
-//d) para o objeto original, adicione o ataque **“Folha Navalha”,** com **45 de dano**, **100 de precisão**, e de tipo **“Grama”**;
+// ```text
+// nome: Investida;
+// dano: 40;
+// tipo: Normal;
+// precisao: 100;
+// ```
+
+// c) Na cópia do objeto original, adicione a propriedade **`ataques`** a partir do **espalhamento** da propriedade criada para o objeto original;
+
+pokemon2.ataques = [...pokemon1.ataques]
+//console.log(pokemon2)
+
+// d) para o objeto original, adicione o ataque **“Folha Navalha”,** com **45 de dano**, **100 de precisão**, e de tipo **“Grama”**;
 
 pokemon1.ataques.push({
-    Nome:"Folha Navalha",
-    Dano: "45 de dano",
-    Precisão: 100,
-    Tipo: "Grama"
-})
+    nome: "Folha Navalha",
+    dano: 45,
+    precisao: 100,
+    tipo: "Grama"}) 
 
-//e) para a cópia, crie o ataque **“Jato de Água”**, com **40 de dano**, **100 de precisão**, e do tipo **“Água”**;
+//console.log(pokemon1)
 
-copiaPokemon1.ataques.push({
-    Nome: "Jato de Água",
-    Dano: "40 de dano",
-    Precisão: 100,
-    Tipo: "Água"
-})
+// e) para a cópia, crie o ataque **“Jato de Água”**, com **40 de dano**, **100 de precisão**, e do tipo **“Água”**;
 
-console.log(pokemon1,copiaPokemon1)
+pokemon2.ataques.push({
+    nome: "Folha Navalha",
+    dano: 45,
+    precisao: 100,
+    tipo: "Grama"}) 
 
+// f) Imprima os dois objetos no console.
 
-
-
-
+console.log(pokemon1)
+console.log(pokemon2)
